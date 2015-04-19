aeb4c29610ac2a94e2dc8f863a69dcfe693a9ab1
Stefan <korun_stefan@hotmail.com>
2015-04-19 15:39:58 +0200

    posle prezentacija HHH

--- js-backend/server.js
+++ js-backend/server.js
@@ -5,18 +5,27 @@ global._ = require('lodash');
 
 var app = require('./app');
 
+
 /* MONGODB */
-//var mongoUrl = 'localhost/soc-pollution';
-var mongoUrl = 'mongodb://korun:korun@ds039261.mongolab.com:39261/soc-pollution';
+var mongoUrl;
+if (process.argv[2] == 'localhost') {
+    mongoUrl = 'localhost/soc-pollution';
+} else if (process.argv[2] == 'network') {
+    mongoUrl = '192.168.0.100:27017/soc-pollution';
+} else {
+    mongoUrl = 'mongodb://korun:korun@ds039261.mongolab.com:39261/soc-pollution';
+}
+console.log('SOC-POLLUTION databese served on', mongoUrl);
 var mongoConn = mongoose.connect(mongoUrl).connection;
 mongoConn.on('error', console.error.bind(console, 'connection error:'));
 mongoConn.once('open', function () {
-    //for (var i = 0; i < 10; i++) {
-    //    app.models.Station.init({name: 'Station' + i, analysis: [{}, {}]}, true);
-    //}
+    // Generate test data
+    //app.models.Station.dummy();
 
     setInterval(function () {
-        console.log(app.reader.getSensorsData());
+        // Read sensor data
+        //app.models.Station.init({name: 'Station' + i, analysis: [{}, {}]}, true);
+        //console.log(app.reader.getSensorsData());
     }, 2000);
 });
 
